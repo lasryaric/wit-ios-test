e6a58a5d0586eebe666f2fffdf875a966bc83098
Aric Lasry <lasry.aric@gmail.com>
2014-10-02 18:46:31 -0700

    Adding the IOS code

---
+++ WITTuto/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  WITTuto
+//
+//  Created by Aric Lasry on 10/2/14.
+//  Copyright (c) 2014 Aric Lasry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Wit/Wit.h>
+
+@interface ViewController : UIViewController <WitDelegate>
+
+
+@end
+
