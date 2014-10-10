73b3c8d25c8adb0d98616f6daf04f6cc1101db3a
Aric Lasry <lasry.aric@gmail.com>
2014-10-10 11:39:38 -0700

    Better UI for the demo app

--- WITTuto/ViewController.h
+++ WITTuto/ViewController.h
@@ -9,6 +9,10 @@
 #import <UIKit/UIKit.h>
 #import <Wit/Wit.h>
 
+static __unused NSString* const kWitAccessToken = @"wit_access_token";
+
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
 @interface ViewController : UIViewController <WitDelegate>
 
 
